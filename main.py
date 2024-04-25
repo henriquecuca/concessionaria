2b2ae56c5a8069525259372bd98b7206d55327a8
henrique <pablocucatv@gmail.com>
2024-04-25 19:39:28 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,11 @@
+lista = []
+lista.append('supra')
+lista.append('honda civic')
+lista.append('gtr r34')
+lista.append('nissan 370z')
+lista.append('audi r8')
+
+for carro in lista:
+    print(carro)
+
+
